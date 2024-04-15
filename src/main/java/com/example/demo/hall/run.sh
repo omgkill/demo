bc0e28aa932bc742378f32b3815a18c53af98879
shuai.jing <15229895912@163.com>
2024-04-15 11:48:57 +0800

    run.sh

--- src/main/java/com/example/demo/hall/run.sh
+++ src/main/java/com/example/demo/hall/run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+if [ -d "/mnt/hgfs/server" ];then echo "hold"; else sudo mount -t fuse.vmhgfs-fuse .host:/ /mnt/hgfs -o allow_other ;fi
+rsync -a --delete  /mnt/hgfs/server/hall /home/deploy/server/
+rsync -a --delete  /mnt/hgfs/server/common /home/deploy/server/
+cp -rf /mnt/hgfs/server/deploy /home/deploy/server/
 
-#cd /home/deploy/server/hall/proto
-#git pull
-
-cp -rf /mnt/hgfs/server/hall /home/deploy/server/
-
-
+#cd ./hall/proto && git pull && cd -
 docker stop game
 cd /home/deploy/server/
 
@@ -14,7 +13,7 @@ docker build . -t game
 
 docker run -d --rm \
 --name game \
---env ENV=test \
+--env ENV=shuai \
 -p 7200:7200 \
 -p 7221:7221 \
 -p 7211:7211 \
